6a4426b8b5a677662dfdf5710a255355b07247ed
Hanra <37299738+HenraL@users.noreply.github.com>
2023-09-08 09:41:22 +0200

    [UPDATE] called token + [UPDATE] the .gitignore

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8", newline="\n") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='colourise-output',
+    name='colourise_output',
     version='1.1.0',
     packages=setuptools.find_packages(),
     install_requires=[
